baa2afdf147703c401ad74ebba8eb46c01ab7dde
Syferr <alvincypher@gmail.com>
2024-04-13 09:34:05 +0000

    first comit

---
+++ src/app/success/page.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react'
+
+const SuccesPage = () => {
+  const searchParams = useSearchParams();
+  const payment_intent = searchParams.get("payment_intent")
+  const router = useRouter()
+  
+  useEffect(() => {
+    const makeRequest = async() => {
+      try {
+        await fetch(`http://localhost:3000/api/confirm/${payment_intent}`, {
+          method: "PUT",
+        });
+        router.push("/orders")
+      } catch (err) {
+        console.log(err)
+      }
+    };
+    makeRequest()
+  }, [payment_intent, router]);
+
+
+  return (
+    <>
+    <div className="min-h-[calc(100vh-6rem)] md:min-h-[calc(100vh-15rem)] flex items-center justify-center text-center text-2xl text-green-700">
+      <p className="max-w-[600px]">
+        Payment successful. You are being redirected to the orders page.
+        Please do not close the page.
+      </p>
+    {/* <ConfettiExplosion className="absolute m-auto"
+    /> */}
+    </div>
+  </>
+  )
+}
+
+export default SuccesPage
---
+++ src/components/Count.tsx
@@ -0,0 +1,51 @@
+
+
+// "use client"
+// import React from 'react'
+// import Countdown from 'react-countdown'
+
+// const endingDate = new Date("2024-04-30")
+
+// const Count = () => {
+//   return (
+//     <Countdown className='font-bold text-5xl text-yellow-300' date={endingDate} />
+//   )
+// }
+
+// export default Count
+
+
+"use client"
+import React, { useState, useEffect } from "react";
+
+const Count = () => {
+  
+  let difference = +new Date(`04/14/2024`) - +new Date();
+  const [delay, setDelay] = useState(difference);
+
+  const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const h = Math.floor((difference / (1000 * 60 * 60)) % 24);
+  const m = Math.floor((difference / 1000 / 60) % 60);
+  const s = Math.floor((difference / 1000) % 60);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDelay(delay - 1);
+    }, 1000);
+
+    if (delay === 0) {
+      clearInterval(timer);
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  });
+  return (
+    <span className="font-bold text-5xl text-yellow-300">
+      {d}:{h}:{m}:{s}
+    </span>
+  );
+};
+
+export default Count;
